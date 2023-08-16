b997e64c3846d709d9c44d0e197b628692112efe
Thisara Kaushan <125348115+thisarakaushan@users.noreply.github.com>
2023-08-16 21:51:48 +0530

    Add files via upload

---
+++ CreateArray.js
@@ -0,0 +1,11 @@
+/* 
+1.Create an array called "fruits" with the following fruits: "apple", "banana", "orange", "grape", "mango".
+
+2.Use JavaScript to display the total number of fruits in the array
+*/
+
+// Create an array called "fruits" with the given fruits
+var fruits = ["apple", "banana", "orange", "grape", "mango"];
+
+// Display the total number of fruits in the array
+console.log("Total number of fruits:", fruits.length);
