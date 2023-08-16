baa22ca41aeeb7e069313fbb3a7cea7abd7d6b3c
Thisara Kaushan <125348115+thisarakaushan@users.noreply.github.com>
2023-08-16 21:55:04 +0530

    Add files via upload

---
+++ PrintSum.js
@@ -0,0 +1,12 @@
+// Create a function that takes two numbers as parameters and returns their sum.
+
+function addNumbers(a, b) {
+    return a + b;
+  }
+  
+  // Example usage
+  var num1 = 5;
+  var num2 = 10;
+  var sum = addNumbers(num1, num2);
+  console.log("The sum is:", sum); // Output: The sum is: 15
+  
